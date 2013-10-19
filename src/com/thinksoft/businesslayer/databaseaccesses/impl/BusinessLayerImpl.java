c10e209fdea870cfd24d18eef6d9bc283e0535a0
unknown <laboratorio@LAB10-13.ulatina.net>
2013-10-18 20:57:08 -0600

    Added git ignore

--- src/com/thinksoft/businesslayer/databaseaccesses/impl/BusinessLayerImpl.java
+++ src/com/thinksoft/businesslayer/databaseaccesses/impl/BusinessLayerImpl.java
@@ -10,7 +10,7 @@ public class BusinessLayerImpl implements BusinessLayer {
 	@Override
 	public void getUserById() {
 		
-
+		user.getUserById();
 	}
 
 }
--- src/com/thinksoft/models/daos/impl/UserDaoImpl.java
+++ src/com/thinksoft/models/daos/impl/UserDaoImpl.java
@@ -1,12 +1,13 @@
 package com.thinksoft.models.daos.impl;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 import com.thinksoft.models.daos.UserDao;
 
 public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void getUserById() {
-		
 	}
 
 }
