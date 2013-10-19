89e482de561bf8e51d834ef2220befb91e24ff5b
unknown <laboratorio@LAB10-13.ulatina.net>
2013-10-18 19:06:50 -0600

    Model and business basic creation

---
+++ src/com/thinksoft/businesslayer/databaseaccesses/impl/BusinessLayerImpl.java
@@ -0,0 +1,16 @@
+package com.thinksoft.businesslayer.databaseaccesses.impl;
+
+import com.thinksoft.businesslayer.databaseaccesses.BusinessLayer;
+import com.thinksoft.models.daos.UserDao;
+import com.thinksoft.models.daos.impl.UserDaoImpl;
+
+public class BusinessLayerImpl implements BusinessLayer {
+	UserDao user = new UserDaoImpl();
+	
+	@Override
+	public void getUserById() {
+		
+
+	}
+
+}
