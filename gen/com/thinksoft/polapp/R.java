856a96d53c4a485d2f78e3f47cf87bb1c5140918
Gustavo Otárola <gob94@outlook.com>
2013-10-20 15:58:59 -0600

    SignIn and Sign Up complete

--- gen/com/thinksoft/polapp/R.java
+++ gen/com/thinksoft/polapp/R.java
@@ -22,33 +22,72 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int key26=0x7f020001;
+        public static final int user26=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btnAccept=0x7f080005;
-        public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080003;
-        public static final int lblPassword=0x7f080002;
-        public static final int lblSignUp=0x7f080004;
-        public static final int lblUserName=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int btnEntrar=0x7f080008;
+        public static final int btnRegistrar=0x7f08000d;
+        public static final int imgPassword=0x7f080004;
+        public static final int imgUserName=0x7f080001;
+        public static final int lblAgregarProductos=0x7f080000;
+        public static final int lblName=0x7f080009;
+        public static final int lblPassword=0x7f080005;
+        public static final int lblPhone=0x7f08000b;
+        public static final int lblSignUp=0x7f080007;
+        public static final int lblUserName=0x7f080002;
+        public static final int txtName=0x7f08000a;
+        public static final int txtPassword=0x7f080006;
+        public static final int txtPhone=0x7f08000c;
+        public static final int txtUserName=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_sign_in=0x7f030001;
-        public static final int activity_sign_up=0x7f030002;
+        public static final int activity_agregar_clientes=0x7f030000;
+        public static final int activity_clientes=0x7f030001;
+        public static final int activity_editar_cliente=0x7f030002;
+        public static final int activity_editar_productos=0x7f030003;
+        public static final int activity_home_screen=0x7f030004;
+        public static final int activity_lista_clientes=0x7f030005;
+        public static final int activity_lista_productos=0x7f030006;
+        public static final int activity_main=0x7f030007;
+        public static final int activity_productos=0x7f030008;
+        public static final int activity_sign_in=0x7f030009;
+        public static final int activity_sign_up=0x7f03000a;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int sign_in=0x7f070001;
-        public static final int sign_up=0x7f070002;
+        public static final int agregar_clientes=0x7f070000;
+        public static final int clientes=0x7f070001;
+        public static final int editar_cliente=0x7f070002;
+        public static final int editar_productos=0x7f070003;
+        public static final int home_screen=0x7f070004;
+        public static final int lista_clientes=0x7f070005;
+        public static final int lista_productos=0x7f070006;
+        public static final int main=0x7f070007;
+        public static final int productos=0x7f070008;
+        public static final int sign_in=0x7f070009;
+        public static final int sign_up=0x7f07000a;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btnAccept=0x7f05000b;
+        public static final int btnRegister=0x7f05000a;
         public static final int hello_world=0x7f050002;
+        public static final int lblAgregarProductos=0x7f050014;
+        public static final int lblName=0x7f050008;
         public static final int lblPassword=0x7f050006;
+        public static final int lblPhone=0x7f050009;
         public static final int lblSignUp=0x7f050007;
         public static final int lblUserName=0x7f050005;
+        public static final int title_activity_agregar_clientes=0x7f05000e;
+        public static final int title_activity_clientes=0x7f05000d;
+        public static final int title_activity_editar_cliente=0x7f050013;
+        public static final int title_activity_editar_productos=0x7f050012;
+        public static final int title_activity_home_screen=0x7f05000c;
+        public static final int title_activity_lista_clientes=0x7f05000f;
+        public static final int title_activity_lista_productos=0x7f050011;
+        public static final int title_activity_productos=0x7f050010;
         public static final int title_activity_sign_in=0x7f050003;
         public static final int title_activity_sign_up=0x7f050004;
     }
